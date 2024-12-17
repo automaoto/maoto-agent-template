62724b765a48fe337716967f19ed18a5df2ad1bd
lennardkorte <mail@lennardkorte.com>
2024-12-17 16:27:31 +0100

    chg 02 provider

--- 02_provider.py
+++ 02_provider.py
@@ -5,22 +5,22 @@ load_dotenv('.secrets_provider')
 
 agent = Maoto()
 
-@agent.register_auth_directive
-def auth_directive(element):
-    if not isinstance(element, Response):
-        raise Exception("This directive can only be used with Response elements.")
+# @agent.register_auth_directive
+# def auth_directive(element):
+#     if not isinstance(element, Response):
+#         raise Exception("This directive can only be used with Response elements.")
     # possibly check if the agent has the rights to send response
 
 @agent.register_response_handler()
 async def response_handler(response: Response):
     print(f"Received response:{response}")
 
-uploaded_files = agent.upload_files([Path("./test_audiofile.mp3")])
+#uploaded_files = agent.upload_files([Path("./test_audiofile.mp3")])
 
 # create a single uuid and get it as a string
-new_post = agent.create_posts(NewPost(
+new_post = agent.create_posts([NewPost(
     description="Book a grab for me please!",
     context=""
-))
+)])
 
 time.sleep(5)
