6cd9721e818e10e2814d1a096b1208aa26ae9693
Lennard Korte <mail@lennardkorte.com>
2025-03-10 23:14:13 +0800

    fix temp

--- 01_resolver_server_register.py
+++ 01_resolver_server_register.py
@@ -10,31 +10,33 @@ maoto_api_key = os.getenv('MAOTO_API_KEY')
 # regsiter maoto webhook
 agent = Maoto(assistant=False)
 
-registeres_actions: list[Action] = agent.get_own_actions()
-registered_actions = [action.get_name() for action in registeres_actions]
-print("Registered actions:", registered_actions)
+# following is temporarily moved somewhere else
 
-agent.delete_actions([action.get_action_id() for action in registeres_actions])
-print("Deleted all registered actions")
+# registeres_actions: list[Action] = agent.get_own_actions()
+# registered_actions = [action.get_name() for action in registeres_actions]
+# print("Registered actions:", registered_actions)
 
-created_actions = agent.create_actions([
-    NewAction(
-        name="grab_ride_hailing",
-        parameters=json.dumps({'longitude': 'int', 'latitude': 'int', 'destination': 'str'}),
-        description="Books a Grab ride.",
-        tags=["Grab", "ride hailing"],
-        cost=None,
-        followup=False
-    ),
-    NewAction(
-        name="tada_ride_hailing",
-        parameters=json.dumps({'longitude': 'int', 'latitude': 'int', 'destination': 'str'}),
-        description="Books a Tada ride.",
-        tags=["Tada", "ride hailing"],
-        cost=None,
-        followup=False
-    ),
-])
+# agent.delete_actions([action.get_action_id() for action in registeres_actions])
+# print("Deleted all registered actions")
+
+# created_actions = agent.create_actions([
+#     NewAction(
+#         name="grab_ride_hailing",
+#         parameters=json.dumps({'longitude': 'int', 'latitude': 'int', 'destination': 'str'}),
+#         description="Books a Grab ride.",
+#         tags=["Grab", "ride hailing"],
+#         cost=None,
+#         followup=False
+#     ),
+#     NewAction(
+#         name="tada_ride_hailing",
+#         parameters=json.dumps({'longitude': 'int', 'latitude': 'int', 'destination': 'str'}),
+#         description="Books a Tada ride.",
+#         tags=["Tada", "ride hailing"],
+#         cost=None,
+#         followup=False
+#     ),
+# ])
 
 agent.set_webhook()
 print("Maoto webhook registered")
