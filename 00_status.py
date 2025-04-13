b4cc1c0dd85630340dbbc51521fb4f1f87789665
Lennard Korte <mail@lennardkorte.com>
2025-04-13 13:58:52 +0800

    fix

--- 00_status.py
+++ 00_status.py
@@ -9,19 +9,13 @@ maoto = Maoto()
 
 async def main():
     try:
-        status_marketplace = await maoto.check_status_marketplace()
-        if not status_marketplace:
-            print("Marketplace is down.")
-    except Exception as e:
+        print(await maoto.health_marketplace())
+    except Exception:
         print("Marketplace is down.")
 
     try:
-        status_assistant = await maoto.check_status_assistant() # note: assistant temporarily not publicly available to save cost
-        if not status_assistant:
-            print("Assistant is down.")
-    except Exception as e:
+        print(await maoto.health_assistant())
+    except Exception:
         print("Assistant is down.")
-
-print("Server is up.")
 
 asyncio.run(main())
